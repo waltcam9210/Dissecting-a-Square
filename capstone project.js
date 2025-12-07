61fefea466210e855b39757a4404e16f9d4fc09b
waltcam9210 <waltcam9210@gmail.com>
2025-12-07 11:15:18 -0500

    upload js

---
+++ capstone project.js
@@ -0,0 +1,42 @@
+for (k=1; k<10001; k++){
+let squareSide = k;
+for (i=1; i<squareSide; i++){
+   let secondLeg=i;
+   for (j=1; j<squareSide; j++){
+    let thirdLeg=j;
+
+    let smallLeg1 = squareSide-secondLeg;
+    let smallLeg2 = squareSide-thirdLeg;
+    let smallHyp = smallLeg1*smallLeg1+smallLeg2*smallLeg2;
+    smallHyp = Math.sqrt(smallHyp);
+    let smallWorks = Number.isInteger(smallHyp);
+
+    let secondHyp = secondLeg**2 + squareSide*squareSide;
+    secondHyp = Math.sqrt(secondHyp);
+    let secondWorks = Number.isInteger(secondHyp);
+
+    let thirdHyp = thirdLeg*thirdLeg + squareSide*squareSide;
+    thirdHyp = Math.sqrt(thirdHyp);
+    let thirdWorks = Number.isInteger(thirdHyp);
+
+    if (smallWorks == true && secondWorks==true && thirdWorks ==true /*&& smallLeg1<smallLeg2*/){
+       for(q=1; q<secondHyp; q++){
+        let newSmallLeg=q;
+        let secondHypMinusSmallLeg= secondHyp - newSmallLeg;
+        let height = Math.sqrt(smallHyp*smallHyp-newSmallLeg*newSmallLeg);
+        let heightWorks = Number.isInteger(height);
+
+        let finalHeightCheck = Math.sqrt(thirdHyp*thirdHyp-secondHypMinusSmallLeg*secondHypMinusSmallLeg)
+
+        if(heightWorks == true && finalHeightCheck == height){
+            console.log("small triangle: "+smallLeg1+ ", "+smallLeg2+", "+smallHyp+ 
+            " second triangle: "+secondLeg+ ", "+squareSide+", "+secondHyp+
+            " third triangle: "+thirdLeg+ ", "+squareSide+", "+thirdHyp+
+            " height: "+height+ " values for w and z-w: "+newSmallLeg+ ", "+secondHypMinusSmallLeg);
+        }
+       }
+   
+    }
+   }
+}
+}
